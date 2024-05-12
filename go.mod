d209b6b1185062b334080ffe5cb46f47a9b569de
Rahul Ghangas <rahulghangas0@gmail.com>
2024-05-12 22:14:37 +0530

    feat: generate c bindings for ffi and generate corresponding go code

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module ezkl-ffi
+
+go 1.21.3
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,10 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 extern crate libc;
 
 mod util;
 
 #[no_mangle]
-pub extern "C" fn verify(
+pub extern "C" fn verify_proof(
     proof_length: libc::size_t,
     proof: *const u8,
     vk_length: libc::size_t,
@@ -12,7 +13,7 @@ pub extern "C" fn verify(
     settings: *const u8,
     srs_length: libc::size_t,
     srs: *const u8,
-) -> libc::__u8 {
+) -> u8 {
     // convert all const u8 to Vec<u8>
     let proof_slice =
         unsafe { std::slice::from_raw_parts(proof as *const u8, proof_length) };
